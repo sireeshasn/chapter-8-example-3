50f2abaa61d87e3f05c72660e0a0ad1d4bf5d456
Sireesha <sireesha2622@gmail.com>
2024-09-09 17:35:10 +1000

    Update push-image.sh

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 #
-# Publishes a Docker image.
+# Pushes the Docker image to Azure Container Registry (ACR).
 #
 # Environment variables:
 #
 #   CONTAINER_REGISTRY - The hostname of your container registry.
-#   REGISTRY_UN - User name for your container registry.
-#   REGISTRY_PW - Password for your container registry.
 #   VERSION - The version number to tag the images with.
 #
 # Usage:
@@ -13,11 +13,14 @@
 #       ./scripts/push-image.sh
 #
 
-set -u # or set -o nounset
+# Treat unset variables as an error
+set -u
+
+# Ensure required environment variables are set
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$REGISTRY_UN"
-: "$REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+# Push the Docker image to the registry
 docker push $CONTAINER_REGISTRY/video-streaming:$VERSION
+
+echo "Docker image pushed successfully!"
